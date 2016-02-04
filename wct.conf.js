974e10b39c0390c3c8e08fcdf384b642e6236d98
Martin Wragg <martin.wragg@ge.com>
2016-02-03 22:41:50 -0800

    add wct conf

--- wct.conf.js
+++ wct.conf.js
@@ -1,14 +1,33 @@
 module.exports = {
-    verbose: true,
-    plugins: {
-        local: {
-            browsers: ['chrome']
-        },
-        sauce: {
-            disabled: true
-        }
+  verbose: true,
+  plugins: {
+    local: {
+      browsers: ['chrome']
     },
-    suites: [
-      'test.html'
-    ]
+    "sauce": {
+      "browsers": [{
+          "browserName": "microsoftedge",
+          "platform": "Windows 10",
+          "version": ""
+        }, {
+          "browserName": "internet explorer",
+          "platform": "Windows 8.1",
+          "version": "11"
+        },
+
+        {
+          "browserName": "safari",
+          "platform": "OS X 10.11",
+          "version": "9"
+        }, {
+          "browserName": "safari",
+          "platform": "OS X 10.10",
+          "version": "8"
+        }
+      ]
+    }
+  },
+  suites: [
+    'test.html'
+  ]
 };
